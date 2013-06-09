4570f87eff141265969defdcab7c2b9cae46292f
Renan Montenegro <renanigt@gmail.com>
2013-06-09 01:30:51 -0300

    Adicionado algumas configurações do JSF. Adicionado também algumas Anotações JPA nos models.

--- src/br/com/estudojsf/model/Endereco.java
+++ src/br/com/estudojsf/model/Endereco.java
@@ -1,7 +1,15 @@
 package br.com.estudojsf.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Endereco {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private String rua;
 	private Long numero;
--- src/br/com/estudojsf/model/Pessoa.java
+++ src/br/com/estudojsf/model/Pessoa.java
@@ -2,11 +2,21 @@ package br.com.estudojsf.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
 public class Pessoa {
 
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private String nome;
 	private Date dataNascimento;
+	@OneToOne
 	private Endereco endereco;
 	
 	public Long getId() {
